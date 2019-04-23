dfdf01cd88c4dadcac3f3eb529c0364dc461e866
Michael <michaelmccabe711@gmail.com>
2019-04-23 19:52:26 -0400

    #6 (14:30) - Created a token when a user successfully signs up on our site using JSON Web Token module

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const JWT = require('jsonwebtoken');
 const { user } = require('../models')
 
 module.exports = {
@@ -22,6 +23,7 @@ module.exports = {
         // If the email exists
         if (DBUser) {
           // status 403 = forbidden
+          // For a user that already exists, I think HTTP status code of 409(Conflict) matches better
           res.status(403).json({ error: "This email is taken" });
         } else {
 
@@ -29,8 +31,19 @@ module.exports = {
           user.create(newUser)
             .then((user) => {
               console.log(user, "successfully added")
+              
+              // res.status(200).json({ data: user.dataValues });
+              const token = JWT.sign({
+                iss: 'Michael',
+                sub: user.id,
+                iat: new Date().getTime(), // current time
+                exp: new Date().setDate(new Date().getDate() + 1) // current time + 1 day ahead
+              }, 'myauthentication');
+
               // Future: Respond with token as well 
-              res.status(200).json({ data: user.dataValues });
+
+              res.status(200).json({ token: token })
+
             })
             .catch((err) => {
               console.log(err, "not successfully added to the database")
