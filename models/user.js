8495ced91b32d4f71e5b6be97a29a3f32a3b281a
Michael <michaelmccabe711@gmail.com>
2019-04-23 19:52:26 -0400

    #6 (14:30) - Created a token when a user successfully signs up on our site using JSON Web Token module

--- models/user.js
+++ models/user.js
@@ -14,6 +14,7 @@ module.exports = function (sequelize, Sequelize) {
             // All incoming emails will be converted to lowercase, before sent to database
             validate: { 
                 isLowercase: true,
+                isEmail: true,
             }
         },
    
