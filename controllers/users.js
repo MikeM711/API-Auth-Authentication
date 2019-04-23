2b80926987b34cc4c4e29b6779f57d7d7b2b18e4
Michael <michaelmccabe711@gmail.com>
2019-04-22 22:34:23 -0400

    7mins thru #4 - using Joi module to create a schema that we will use for authentication of our signup

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,11 @@
 module.exports = {
   signUp: async (req,res,next) => {
+    // Email & Password
     console.log('UsersController.signUP() called!')
   },
 
   signIn: async (req,res,next) => {
+    // Generate Token
     console.log('UsersController.signIn() called!')
   },
 
