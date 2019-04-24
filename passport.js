f622037c2ff102818ba75e11cbe133fbacedb735
Michael <michaelmccabe711@gmail.com>
2019-04-24 09:44:44 -0400

    #7 (end) - installed and used passport-local to handle when a user SIGNS IN. We created a check to see if the user that signs in has an email found inside our database.  We have stopped at the part where we need to check if the password is correct - because we will be using bCrypt in the future to hash/salt passwords.

--- passport.js
+++ passport.js
@@ -1,10 +1,11 @@
 const passport = require('passport')
 const JwtStrategy = require('passport-jwt').Strategy;
 const { ExtractJwt } = require('passport-jwt')
+const LocalStrategy = require('passport-local').Strategy
 const { user } = require('./models')
-
 const { JWT_SECRET } = require('./config')
 
+// JSON WEB TOKEN STRATEGY
 passport.use(new JwtStrategy({
   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
   secretOrKey: JWT_SECRET
@@ -29,6 +30,36 @@ passport.use(new JwtStrategy({
     })
     .catch(err => {
       console.log('Error: ', err)
-      done(error,false);
+      done(error, false);
     })
 }));
+
+// LOCAL STRATEGY
+passport.use(new LocalStrategy({
+  usernameField: 'email'
+}, (email, password, done) => {
+
+  // Find the user, given the email
+  user.findOne({
+    where: {
+      email: email
+    }
+  })
+    .then(currUser => {
+      // If user doesn't exist, handle it
+      if (!currUser) {
+        return done(null, false)
+      }
+
+      // Check if the password is correct
+
+      // If not, handle it
+
+      // Otherwise, return the user
+
+    })
+    .catch(err => {
+      console.log('User not found in DB', err)
+    })
+
+}))
