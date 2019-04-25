f4a08094d57d5650da06a8b912d25fd9e9bee112
Michael <michaelmccabe711@gmail.com>
2019-04-24 20:23:07 -0400

    #9 (beginning) - moving files. Putting all server files in server folder.  Starting application with index.js.

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan')
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//Middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// Routes
+app.use('/users', require('./routes/users'))
+
+module.exports = app;
