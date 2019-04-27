41618b9a121e19b75099f65d892fa69d236c234b
Michael <michaelmccabe711@gmail.com>
2019-04-26 23:31:51 -0400

    #14 (end) - Deleted unnecessary create-react-app files. Initialized bootstrap, used its css for navbar. Installed react-router-dom for use of BrowserRouter and Route, and Link for no refreshing. Created all necessary components. Passed in all Route components inside App component as props.

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  render(){
+    return (
+      <div>
+        This is a Dashboard component
+      </div>
+    );
+  };
+};
+
+export default Dashboard
---
+++ client/src/components/SignUp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+  render(){
+    return (
+      <div>
+        This is a SignUp component
+      </div>
+    );
+  };
+};
+
+export default SignUp
