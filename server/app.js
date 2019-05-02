052609d80c2e3b20c10d3df594c5d1f26f9bcd93
Michael <michaelmccabe711@gmail.com>
2019-05-02 19:56:02 -0400

    Added corsOptions to cors server-side, for more security - only one domain can share data with the backend - http://localhost:3000

--- server/app.js
+++ server/app.js
@@ -5,7 +5,12 @@ const cors = require('cors')
 
 const app = express();
 
-app.use(cors());
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 
 //Middleware
 app.use(morgan('dev'));
