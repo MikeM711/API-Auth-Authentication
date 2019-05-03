0e0b8a9143ed10ec37e0ad59e449b17a9ad00409
Michael <michaelmccabe711@gmail.com>
2019-05-03 08:34:47 -0400

    no more database URL

--- server/models/index.js
+++ server/models/index.js
@@ -6,13 +6,14 @@ var Sequelize = require('sequelize');
 var basename = path.basename(__filename);
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'production') {
-  var sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-  });
- }
+// if (env === 'production') {
+//   var sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: 'postgres',
+//     protocol: 'postgres',
+//   });
+//  }
  
+// Add this to config.json: "use_env_variable": "DATABASE_URL", in PRODUCTION
 
 var config = require(__dirname + '/../config/config.json')[env];
 var db = {};
